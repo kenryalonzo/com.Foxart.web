442fe410a313daffa730e93705206a4bae6a7547
FOTIE FOTIE NATHANAEL <kenryalonzo@gmail.com>
2025-04-03 19:00:49 +0100

    creation et utilisation du component Container

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import {logoApp} from "../assets/index"
 
 import React from 'react'
+import { Container } from "./Container"
 
 const Header = () => {
   return (
     <div className='border-b border-x-stroke-1'>
-      <img src={logoApp} width={104} height={32}  alt="App logo" />
-      <button>login</button>
+      <Container>
+        <img src={logoApp} width={104} height={32}  alt="App logo" />
+        <button>login</button>
+      </Container>
     </div>
   )
 }
