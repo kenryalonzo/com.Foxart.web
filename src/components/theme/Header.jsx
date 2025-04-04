2bbe023bd918b03c6f9300c041adca7f7c8d0bb2
FOTIE FOTIE NATHANAEL <kenryalonzo@gmail.com>
2025-04-04 19:08:08 +0100

    fix: correct the structure and fix errors.

--- src/components/theme/Header.jsx
+++ src/components/theme/Header.jsx
@@ -4,7 +4,7 @@ export const ElipseBlueBlur = () => {
 
   return (
     <img src={elipseBlueBlur} alt="elipse-Blue-Blur" 
-        className="absolute -top-[14rem] -left-[9rem] z-0 pointer-events-none" 
+        className="absolute -top-[14rem] -left-[9rem] z-0 pointer-events-none mix-blend-color-dodge" 
     />
   )
 }
