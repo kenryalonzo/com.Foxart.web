1d4a7bd5b0f71022e086fe0f2be9e2c4c913c234
FOTIE FOTIE NATHANAEL <kenryalonzo@gmail.com>
2025-04-05 23:48:23 +0100

    feat: util for bulletPoint

---
+++ src/components/theme/Headings.jsx
@@ -0,0 +1,14 @@
+import { Container } from "../Container"
+
+const Headings = ({ title, subtitle }) => {
+  return (
+        <Container className={"py-5 lg:py-10"}>
+            <div className="caption-1 text-n-5 text-center uppercase">
+                {title}
+            </div>
+            <h2 className="h2 text-center">{subtitle}</h2>
+        </Container>
+  );
+};
+
+export default Headings
