3ad46eccc08faca329d8746d3b5a87de333591f9
FOTIE FOTIE NATHANAEL <kenryalonzo@gmail.com>
2025-04-08 15:49:41 +0100

    Pwa App

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024 
+      },
+      manifest: false, 
+    })
+  ],
 })
